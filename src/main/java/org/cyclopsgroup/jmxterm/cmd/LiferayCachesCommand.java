f34cf42ab40e5fefc945c4e1ed1548fb712b8f36
Sampsa Sohlman <sampsa@sohlman.com>
2017-06-07 12:25:20 +0300

    Add support for LiferayCaches SQL insert and defiing misspercentage

--- src/main/java/org/cyclopsgroup/jmxterm/cmd/LiferayCachesCommand.java
+++ src/main/java/org/cyclopsgroup/jmxterm/cmd/LiferayCachesCommand.java
@@ -17,7 +17,9 @@ import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
 import org.apache.commons.collections.map.ListOrderedMap;
+import org.cyclopsgroup.jcli.annotation.Argument;
 import org.cyclopsgroup.jcli.annotation.Cli;
+import org.cyclopsgroup.jcli.annotation.Option;
 import org.cyclopsgroup.jmxterm.Command;
 import org.cyclopsgroup.jmxterm.Session;
 import org.cyclopsgroup.jmxterm.io.ValueOutputFormat;
@@ -34,7 +36,13 @@ public class LiferayCachesCommand extends Command {
 	private String delimiter = "";
 
 	private boolean simpleFormat = true;
+	
+	private String format = "list";
+	
+	private String serverName = "serverName";	
 
+	private int misspercentage = 0;
+	
 	public static List<String> getLiferayStatisticBeans(Session session, String domainName)
 			throws MalformedObjectNameException, IOException {
 		ObjectName queryName = null;
@@ -85,19 +93,27 @@ public class LiferayCachesCommand extends Command {
 			}
 		});
 		
-		//String format = "%s\t%s\t%s\t%s\t%s\t";
-		String format = "%1$-27s %2$-130s %3$12s %4$12s %5$12s %6$20s";
-		
-		
-		
-		session.output.println(String.format(format, fields[0], fields[1],fields[2],fields[3],fields[4], fields[5]));
-		for(String[] s : stringsList) {
+		if (this.format!=null && "sql".equals(this.format)) {
+			//String format = "%s\t%s\t%s\t%s\t%s\t";
+			String printformat = "INSERT INTO CacheSizes VALUES (\"%s\",\"%s\",\"%s\",%s,%s,%s,%s);";
 			
-			int misspercentage = Integer.valueOf(s[5]).intValue();
 			
-			if (misspercentage > 25) {
-				session.output.println(String.format(format,s[0], s[1],s[2],s[3],s[4],s[5]));
+			for(String[] s : stringsList) {
+				session.output.println(String.format(printformat, this.serverName, s[0], s[1],s[2],s[3],s[4],s[5]));
 			}
+		}
+		else {
+			String printformat = "%1$-27s %2$-130s %3$12s %4$12s %5$12s %6$20s";
+			
+			session.output.println(String.format(printformat, fields[0], fields[1],fields[2],fields[3],fields[4], fields[5]));
+			for(String[] s : stringsList) {
+				
+				int misspercentage = Integer.valueOf(s[5]).intValue();
+				
+				if (misspercentage >= this.misspercentage) {
+					session.output.println(String.format(printformat,s[0], s[1],s[2],s[3],s[4],s[5]));
+				}
+			}			
 		}
 	}
 	
@@ -167,7 +183,37 @@ public class LiferayCachesCommand extends Command {
 		*/
 	}
 
-	
+
+	    /**
+	     * @param report True to output result line by line as report
+	     */
+	    @Option( name = "f", longName = "format", description = "Format <list|sql>", defaultValue="list")
+	    public final void setFormat( String format )
+	    {
+	        this.format = format;
+	    }
+    
+   /**
+    * Set bean option
+    *
+    * @param bean Bean to set
+    */
+   @Option( name = "s", longName = "server", description = "Servername for SQL printout", defaultValue="server" )
+   public final void setServerName( String serverName )
+   {
+       this.serverName = serverName;
+   }
+   
+   /**
+    * Set bean option
+    *
+    * @param bean Bean to set
+    */
+   @Option( name = "mp", longName = "misspercentage", description = "Prints only that miss percentage is larger", defaultValue="0" )
+   public final void setMissPercentage( int misspercentage )
+   {
+       this.misspercentage = misspercentage;
+   }
 	
 	/**
 	 * @inheritDoc
